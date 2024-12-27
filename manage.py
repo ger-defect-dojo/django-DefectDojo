779a32be5a8a16a99d71719e09b9faacad93de14
German Chaverri <german@defectdojo.com>
2024-12-27 10:14:09 -0600

    Enable debugpy on port 3000

--- manage.py
+++ manage.py
@@ -5,6 +5,12 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dojo.settings.settings")
 
+    if os.environ.get("DEBUG_TEST"):
+        import debugpy
+
+        debugpy.listen(("0.0.0.0", 3000))
+        print("Attached!")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
